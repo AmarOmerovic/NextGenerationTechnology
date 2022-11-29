28002db536b1896d337841942edeee017044240a
AmarOmerovic <amaromerovic473@gmail.com>
2022-11-29 19:38:18 +0100

    add footer structure & fix some bugs

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
+const welcome = document.querySelector('welcome')
+
+
+window.onbeforeunload = function () {
+    window.scrollTo(0,0);
+};
+
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+setTimeout(function() {
+    document.body.style.overflowY = "scroll";
+}, 5200);
+
+window.onunload = function(){ window.scrollTo(0,0); }
