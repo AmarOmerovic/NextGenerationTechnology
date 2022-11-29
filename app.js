281883e05ff1ee83b1b752c54a13be10c13695a8
AmarOmerovic <amaromerovic473@gmail.com>
2022-11-30 00:06:12 +0100

    final touch and some more responsive buf fixes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 
-
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
@@ -9,6 +8,7 @@ menu.addEventListener('click', function() {
 
 setTimeout(function() {
     document.body.style.overflowY = "scroll";
+
 }, 5000);
 
 
